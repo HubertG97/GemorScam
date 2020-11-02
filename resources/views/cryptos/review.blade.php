0a74e840932439a05632c84761ba80a6570ec67d
Hubert <hubertgetrouw@gmail.com>
2020-11-02 16:49:22 +0100

    pub

--- resources/views/cryptos/review.blade.php
+++ resources/views/cryptos/review.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-8">
                 @foreach($all_cryptos as $crypto)
                     <div class="flex flex-row bg-white rounded shadow p-3 mb-3">
-                        <div style="background-image: url({{ asset('public/image/logo/' . $crypto->logo_url) }}); background-position:center; background-repeat: no-repeat; background-size: contain;" class="w-3/12 h-48"></div>
+                        <div style="background-image: url({{ asset('/image/logo/' . $crypto->logo_url) }}); background-position:center; background-repeat: no-repeat; background-size: contain;" class="w-3/12 h-48"></div>
                         <div class="w-7/12 p-3">
                             <div class="flex flex-row justify-between"><h3 class="">{{$crypto->name}} ({{$crypto->ticker}})</h3><p><i>by {{$crypto->User->name}}</i></p></div>
                             <p class="multi-line">{{$crypto->description}}</p>
