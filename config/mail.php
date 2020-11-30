aedbbe0fad96576c749d338e7a2023a0f83c3da9
Hubert <hubertgetrouw@gmail.com>
2020-11-30 17:30:18 +0100

    mailo

--- config/mail.php
+++ config/mail.php
@@ -83,10 +83,9 @@ return [
     |
     */
 
-    'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
-    ],
+    'from' => ['address' => 'no-reply@gemorscam.com', 'name' => 'GemOrScam'],
+
+
 
     /*
     |--------------------------------------------------------------------------
